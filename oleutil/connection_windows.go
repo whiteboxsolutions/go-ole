b9727eb541d3564c486bbdabbf292bf10fdbc7e9
Jake Daynes <jake@whitebox.solutions>
2024-07-08 14:35:04 -0700

    Switched all deps to whitebox

--- oleutil/connection_windows.go
+++ oleutil/connection_windows.go
@@ -7,7 +7,7 @@ import (
 	"syscall"
 	"unsafe"
 
-	ole "github.com/go-ole/go-ole"
+	ole "github.com/whiteboxsolutions/go-ole"
 )
 
 // ConnectObject creates a connection point between two services for communication.
